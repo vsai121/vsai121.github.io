c1c0203a4c1d9a19b6c13bf2a403fe32b6989d89
Saicharan <vsaicharan1998@gmail.com>
2019-12-08 14:32:35 +0530

    Changes

--- static/js/animation.js
+++ static/js/animation.js
@@ -119,7 +119,28 @@ function toggleAccordion(){
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
 }
+jQuery(document).ready(function($){
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+   		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
 
+//open popup
+function openpopup(id) {
+    event.preventDefault();
+    	$("#"+id+"").addClass('is-visible');
+}
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
